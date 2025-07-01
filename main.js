834600bb66901200b336bafa7ba906d5f40e988d
Scokin <168649225+Scokin@users.noreply.github.com>
2025-07-01 12:20:20 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow, dialog, ipcMain } = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+ipcMain.handle('select-folder', async () => {
+  const result = await dialog.showOpenDialog({
+    properties: ['openDirectory']
+  });
+  return result.filePaths[0];
+});
+
+ipcMain.handle('read-directory', async (_, folderPath) => {
+  function walk(dir) {
+    let results = [];
+    const list = fs.readdirSync(dir);
+    list.forEach(file => {
+      const fullPath = path.join(dir, file);
+      const stat = fs.statSync(fullPath);
+      if (stat && stat.isDirectory()) {
+        results = results.concat(walk(fullPath));
+      } else {
+        results.push({
+          path: fullPath,
+          name: file,
+          size: stat.size,
+          ext: path.extname(file).toLowerCase()
+        });
+      }
+    });
+    return results;
+  }
+
+  return walk(folderPath);
+});
